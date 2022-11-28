431c6a30a1927715ccf3cd1090314e82981bfe48
Mantas <mantaspauza@gmail.com>
2022-11-28 21:05:15 +0200

    style: :art: add onload animation
    added onload animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,18 @@ import './App.css';
 import { Login } from './components/Login';
 import { Register } from './components/Registration';
 import { Container } from 'react-bootstrap';
+import { useFadeIn } from './custom_hooks/useFadeIn';
+import { useRef, useState } from 'react';
+
+
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const ref = useRef();
+  useFadeIn(ref, 1000);
   return (
-    <Container id='form_container' className={'d-flex flex-column w-50 gap-2 align-items-center'} >
-      <Login/>
+    <Container ref={ref} id='form_container' className={'d-flex flex-column w-50 gap-2 align-items-center'} >
+      <Login show={showLogin} />
       <Register />
     </Container>
   );
